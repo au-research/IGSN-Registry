8e70b303b86468dddfddc4e79d8a28a11379897b
Liz Woods <liz.woods@ands.org.au>
2018-07-19 12:36:15 +1000

    added the database update alter table command to add the tc_accepted boolean field to the registrant table CC-2240

---
+++ sql/IGSN-Postgres_R28-R28.1.sql
@@ -0,0 +1,8 @@
+﻿﻿-- Column: version30.registrant.tc_accepted
+
+-- ALTER TABLE version30.registrant DROP COLUMN tc_accepted;
+
+--add the boolean flag for acceptance of terms and conditions.
+
+ALTER TABLE version30.registrant
+    ADD COLUMN tc_accepted boolean NOT NULL DEFAULT false;
