76553ff1e49cbb244470ec7bbb5e61018e9ed798
Leo Monus <leo.monus@anu.edu.au>
2019-07-05 15:29:38 +1000

    CC-2558

--- src/main/java/org/csiro/igsn/utilities/MailUtils.java
+++ src/main/java/org/csiro/igsn/utilities/MailUtils.java
@@ -28,7 +28,7 @@ public class MailUtils {
     public MailUtils(){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:../applicationContext.xml");
         this.mailSender = (JavaMailSender) ctx.getBean("MyMailSender");
-        this.fromEmail = "services@ands.org.au";
+        this.fromEmail = "services@ardc.edu.au";
     }
 
     public void sendMail(String subject, String bodyText, String toAddress) {
@@ -60,17 +60,15 @@ public class MailUtils {
                 + "<html>"
                 + "<head>"
                 + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><title>IGSN Event Notification email</title></head><body>"
-                 + "<p>Your IGSN has been successfully minted through the ANDS IGSN service.</p>"
+                 + "<p>Your IGSN has been successfully minted through the ARDC IGSN service.</p>"
                  + "<p>If the visibility of the metadata is set to public, it will be viewable at the following URL: <a href='" + igsnUrl + "'>" + igsnUrl + "</a></p>"
                  + "<p>To make changes to your IGSN you will need to <a href='"+AAFLoginUrl+"'>login</a></p>"
-                 + "<p>If you have any questions regarding the service, please visit the ANDS <a href='http://www.ands.org.au/online-services/igsn-service'>website</a>"
-                 + " or contact <a href='mailto:services@ands.org.au'>services@ands.org.au</a>.</p>"
-                 + "<p><b>Australian National Data Service</b><br/>"
-                 + "E: <a href='mailto:services@ands.org.au'>services@ands.org.au</a> | W: <a href='http://ands.org.au'>ands.org.au</a></p>"
+                 + "<p>If you have any questions regarding the service, please visit the ARDC <a href='https://ardc.edu.au/services/identifier/igsn/'>website</a>"
+                 + " or contact <a href='mailto:services@ardc.edu.au'>services@ardc.edu.au</a>.</p>"
+                 + "<p><b>Australian Research Data Commons</b><br/>"
+                 + "E: <a href='mailto:services@ardc.edu.au'>services@ardc.edu.au</a> | W: <a href='https://ardc.edu.au'>ardc.edu.au</a></p>"
                  + "<p>Physical address: 9 Liversidge Street, Australian National University, Acton, ACT 2601<br/>"
                  + "Postal address: 101 Liversidge Street, Australian National University, Acton, ACT 2601<br/>"
-                 + "<a href='http://www.ands.org.au/about-us/ands-nectar-rds'>Working in partnership with Nectar and RDS</a></p>"
-                 + "<a href='http://www.ands.org.au/about-us/ands-nectar-rds'><img src='http://www.ands.org.au/__data/assets/image/0008/1001600/ands-nectar-rds-200px.png'/></a>"
                  + "<p><i>Please consider the environment before printing this e-mail.<i></body></html>";
     }
 
